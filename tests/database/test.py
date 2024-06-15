01d14d4e858739392cc92e399c97195df5cf1505
unknown <maxpline83@gmail.com>
2024-06-15 02:52:37 +0200

    remove the logs from sql

--- tests/database/test.py
+++ tests/database/test.py
@@ -1,8 +1,7 @@
 import unittest
-import os
 from src.database.database import DatabaseConnector
 
-TEST_DB_NAME = 'api_requests.db'
+TEST_DB_NAME = 'test_api_requests.db'
 
 
 class TestDatabaseConnector(unittest.TestCase):
@@ -11,24 +10,15 @@ class TestDatabaseConnector(unittest.TestCase):
     def setUpClass(cls):
         # Initialize the DatabaseConnector for testing
         cls.db_connector = DatabaseConnector(db_name=TEST_DB_NAME)
-        # cls.db_connector.initialize_database()
-
-
-    @classmethod
-    def tearDownClass(cls):
-        os.remove(TEST_DB_NAME)
+        cls.db_connector.initialize_database()
 
     def setUp(self):
         self.db_connector.insert_example_data_db()
-
-    def tearDown(self):
-        self.db_connector.remove_all_data_db()
 
     def test_insert_and_read_data(self):
         results = self.db_connector.read_db()
         self.assertEqual(len(results), 3)
 
-        # Check specific data points
         self.assertEqual(results[0].query, '5 3 +')
         self.assertEqual(results[0].result, 8.0)
 
@@ -39,10 +29,10 @@ class TestDatabaseConnector(unittest.TestCase):
         self.assertEqual(results[2].result, 5.0)
 
     def test_remove_all_data(self):
-        # Test removal of all data
+
         self.db_connector.remove_all_data_db()
         results = self.db_connector.read_db()
-        self.assertEqual(len(results), 0)  # Expecting no records after deletion
+        self.assertEqual(len(results), 0)
 
 
 if __name__ == '__main__':
